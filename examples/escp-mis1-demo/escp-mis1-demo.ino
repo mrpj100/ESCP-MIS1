5d121771efb985702ccceae558cda4c8d280aef7
Mike Prior-Jones <github@randominformation.co.uk>
2021-06-29 13:42:54 +0100

    Adds example file, examples/escp-mis1-demo/escp-mis1-demo.ino

---
+++ examples/escp-mis1-demo/escp-mis1-demo.ino
@@ -0,0 +1,31 @@
+#include "ESCP-MIS1.h"
+
+ESCP-MIS1 sensor;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(19200);
+
+  sensor.setFluidDensity(997); // kg per cubic metre - 997 is the value for fresh water
+  sensor.setAtmosphericPressure(1.000); // sets default atmospheric pressure (used for depth calculation) to 1 bar (= 1000 hPa)
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  Serial.print("Pressure: ");
+  Serial.print(sensor.pressure());
+  Serial.println(" bar");
+
+  Serial.print("Temperature: ");
+  Serial.print(sensor.temperature());
+  Serial.println(" deg C");
+
+  Serial.print("Depth below surface: ");
+  Serial.print(sensor.depth());
+  Serial.println(" metres");
+
+  delay(5000);
+}
